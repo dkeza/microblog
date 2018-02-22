ec75e76a0c1bbe97a450c9ea1f7d1aa3f10f84d9
keza <dkezic@yahoo.com>
2018-02-22 21:25:25 +0100

    Add flask-babel module

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,8 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,8 +22,13 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
